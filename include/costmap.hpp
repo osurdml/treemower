4a3fa90f6dea5bfe0958e2e13ee08dd6b994c05a
Soo-Hyun Yoo <yoos117@gmail.com>
2014-08-13 15:24:17 -0700

    Comment costmap functions.

--- include/costmap.hpp
+++ include/costmap.hpp
@@ -26,14 +26,57 @@ class CostMap {
 public:
 	CostMap(const char *cm_filename, long rows, long cols, long lookahead);
 	CostMap(CostMap *cm);
+
+	/**
+	 * @brief Get map size.
+	 *
+	 * @return Number of rows and columns.
+	 */
 	std::pair<long, long> getSize(void);
+
+	/**
+	 * @brief Copy all data from another CostMap.
+	 *
+	 * @param cm Pointer to another CostMap from which to copy data.
+	 */
 	void copyFrom(CostMap *cm);
+
+	/**
+	 * @brief Get score at given coordinates.
+	 *
+	 * @param x X coordinate.
+	 * @param y Y coordinate.
+	 *
+	 * @return The score if coordinates are in bounds, -1 otherwise.
+	 */
 	float getScore(long x, long y);
+
+	/**
+	 * @brief Set score at given coordinates.
+	 *
+	 * @param x X coordinate.
+	 * @param y Y coordinate.
+	 * @param score Score to set.
+	 *
+	 * @return 0 on success, -1 otherwise.
+	 */
 	int setScore(long x, long y, float score);
 
-	// Backtrack num_steps number of action steps, forwards or backwards.
+	/**
+	 * @brief Step active map forward or backward in history.
+	 *
+	 * Will unconditionally step forward. If requested number of steps backward
+	 * exceeds the number of remaining undos, no steps will be taken at all.
+	 *
+	 * @param num_steps Number of steps to take. Positive or negative for forward or backward, respectively.
+	 *
+	 * @return Number of steps actually taken.
+	 */
 	int Step(int num_steps);
 
+	/**
+	 * @brief Print debug information.
+	 */
 	void PrintDebug(void);
 };
 
