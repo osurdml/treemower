65d03d9e1a9e6a3a30a01811dfeb39029d5d0955
Soo-Hyun Yoo <yoos117@gmail.com>
2014-08-15 19:42:14 -0700

    refs #13 Add Lawnmower as its own class.

---
+++ include/lawnmower.hpp
@@ -0,0 +1,22 @@
+#ifndef LAWNMOWER_HPP
+#define LAWNMOWER_HPP
+
+#include <costmap.hpp>
+#include <decisiontree.hpp>
+
+#include <stdlib.h>
+#include <vector>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+
+class Lawnmower : public DecisionTree {
+	static long AddDecision(std::vector<state_t> *states, long x, long y, float score);
+	long Explore(state_t *state, std::vector<state_t> *states);
+	vx_t FindBest(vx_t source_vx);
+
+public:
+	Lawnmower(const char *cm_filename, long rows, long cols, int lookahead);
+};
+
+#endif // LAWNMOWER_HPP
+
---
+++ src/lawnmower.cpp
@@ -0,0 +1,74 @@
+#include <lawnmower.hpp>
+
+#include <stdio.h>
+
+Lawnmower::Lawnmower(const char *cm_filename, long rows, long cols, int lookahead) :
+	DecisionTree(cm_filename, rows, cols, lookahead)
+{
+}
+
+long Lawnmower::AddDecision(std::vector<state_t> *states, long x, long y, float score)
+{
+	state_t d = {{x, y}, score, 0};
+	states->push_back(d);
+
+	return 1;
+}
+
+long Lawnmower::Explore(state_t *state, std::vector<state_t> *states)
+{
+	long x = state->loc.x;
+	long y = state->loc.y;
+	long nc = 0;
+
+	// Calculate score at current location
+	state->score = 0;
+
+	if (cm.GetCost(x,y+1) > 0) {
+		// Up
+		//std::cout << "Up\n";
+		nc += AddDecision(states, x, y+1, state->score);
+	}
+	else if (cm.GetCost(x,y-1) > 0) {
+		// Down
+		//std::cout << "Down\n";
+		nc += AddDecision(states, x, y-1, state->score);
+	}
+	else if (cm.GetCost(x+1,y) > 0) {
+		// Across
+		//std::cout << "Across\n";
+		nc += AddDecision(states, x+1, y, state->score);
+	}
+	else {
+		// End
+		//std::cout << "End\n";
+	}
+
+	cm.SetCost(x, y, 0);   // TODO(yoos): Use ScoreMap (or InfoMap, whatever) here.
+
+	// Debug
+	//cm.PrintDebug();
+
+	return nc;
+}
+
+vx_t Lawnmower::FindBest(vx_t source_vx)
+{
+	vx_t best_vx = source_vx;
+	float best_score = dTree[source_vx].state.score;
+
+	if (boost::out_degree(source_vx, dTree) > 0) {
+		std::pair<edge_iter, edge_iter> edges = boost::out_edges(source_vx, dTree);
+		for(; edges.first != edges.second; edges.first++) {
+			vx_t child_vx = boost::target(*edges.first, dTree);
+			float child_score = dTree[FindBest(child_vx)].state.score;
+			if (child_score >= best_score) {
+				best_vx = child_vx;
+				best_score = child_score;
+			}
+		}
+	}
+
+	return best_vx;
+}
+
