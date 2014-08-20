10638c65af7c6a2b74919101a775290401e323a2
Soo-Hyun Yoo <yoos117@gmail.com>
2014-08-20 16:54:32 -0700

    refs #13 Fix minor issue with creatmap.

--- Matlab-Version/createmap.m
+++ Matlab-Version/createmap.m
@@ -5,6 +5,6 @@ goalY = MapSize;
 goalZ = 1;
 [dc,cost,Z2]=MapH(MapSize,5,20,goalX,goalY,goalZ); % you can change 5 and 20 to get different map, just play with it and see what do you need.
 cost = 10*cost;
-csvwrite('scoremap.csv','cost');
+csvwrite('scoremap.csv',cost);
 figure;
 contour(cost);
