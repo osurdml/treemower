1ec0bccc265599c9264dbcdb6160581663aab79d
Soo-Hyun Yoo <yoos117@gmail.com>
2014-08-28 17:37:05 -0700

    Multiprocess autotester.

--- multitest.py
+++ multitest.py
@@ -16,47 +16,55 @@ NUM_PROC = 10
 # Stuff to permutate
 scoremaps = ['costmap', 'costmap2']
 algorithms = ['lm', 'rh']
-lookaheads = [2, 3, 4, 5, 6, 7, 8]
+lookaheads = [2, 3, 4, 5]
 rand_fracs = [0.1, 0.2, 0.5, 1.0]
 
-# Generate argument list
+def args2fn(arglist):
+    return OUTPUT_DIR + "/" + arglist['sm'] + "_" \
+            + arglist['alg'] + "_" \
+            + str(arglist['la']) + "_" \
+            + str(arglist['budget']) + "_" \
+            + str(arglist['frac']) + ".txt"
+
+def args2cmd(arglist):
+    return './treemower ' \
+            + DATA_DIR + "/" + arglist['sm'] + ".txt " \
+            + arglist['alg'] + " " \
+            + str(arglist['la']) + " " \
+            + str(arglist['budget']) + " " \
+            + str(arglist['frac']) + " " \
+            + args2fn(arglist)
+
+def args2csv(arglist):
+    return arglist['sm'] + ', ' \
+            + str(arglist['la']) + ', ' \
+            + str(arglist['budget']) + ', ' \
+            + str(arglist['frac'])
+
+def run(mp_args):
+    cmd = args2cmd(mp_args)
+    score = os.popen(cmd).read()
+
+    # Print progress
+    with mp_lock:
+        print(str(mp_progress.value)+'/'+str(len(p_list))+':', cmd)
+        mp_progress.value += 1
+
+    return score
+
+
+# Permute arguments
 p_list = []
-order_map = {}
-order = 0
 for sm in scoremaps:
     for la in lookaheads:
         for f in rand_fracs:
-            res = sm + ', ' + str(la) + ', ' + str(BUDGET) + ', ' + str(f)
+            arglist = {'sm': sm, 'la': la, 'budget': BUDGET, 'frac': f}
             for alg in algorithms:
-                # Record generated path to output dir.
-                alg_fn = OUTPUT_DIR + "/" + sm + "_" + alg + "_" + str(la) + "_" + str(BUDGET) + "_" + str(f) + ".txt";
-                alg_args = DATA_DIR + "/" + sm + ".txt " + alg + " " + str(la) + " " + str(BUDGET) + " " + str(f) + " " + alg_fn
-
-                p_list.append([res, alg_args])
-                order_map[alg_args] = order
-
-            order += 1
-
-# HACK
-score_map = {}
-
-def run(mp_args):
-    #score = os.popen("./treemower " + mp_args[1]).read()
-
-    idx = order_map[mp_args[1]]
-    score_map[idx] = 
-
-    # Print progress
-    print(str(mp_progress.value)+'/'+str(len(p_list))+':', mp_args[0])
-    mp_progress.value += 1
-
-    # Add result to table
-
+                arglist['alg'] = alg
+                p_list.append(arglist.copy())
 
 # Open results file.
 res_f = open(RESULT_FN, 'wb')
-#p_count = 1
-mp_progress = multiprocessing.Value('i', 1)
 
 # Write header
 header = "Map, Lookahead, Budget, Random fraction"
@@ -65,18 +73,25 @@ for alg in algorithms:
 print(header, file=res_f)
 
 # Run algorithms
-lock = multiprocessing.Lock()
+mp_lock = multiprocessing.Lock()
+mp_progress = multiprocessing.Value('i', 1)
 mp = multiprocessing.Pool(NUM_PROC)
-mp.map(run, p_list)
+mp_results = mp.map(run, p_list)
 mp.close()
 mp.join()
 
-# Record results
-# TODO(yoos)
+# Write results
+res_idx = 0
+for sm in scoremaps:
+    for la in lookaheads:
+        for f in rand_fracs:
+            arglist = {'sm': sm, 'la': la, 'budget': BUDGET, 'frac': f}
+            res_f.write(args2csv(arglist))
+            for alg in algorithms:
+                res_f.write(', '+mp_results[res_idx])
+                res_idx += 1
+            res_f.write('\n')
 
 # Close results file.
 res_f.close()
-
-
-
 
