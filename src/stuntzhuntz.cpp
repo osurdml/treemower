1466906d76b3daf05870560fd28153194704dc49
Soo-Hyun Yoo <yoos117@gmail.com>
2014-09-22 15:23:50 -0700

    Enable constant step size in stuntzhuntz.

--- src/stuntzhuntz.cpp
+++ src/stuntzhuntz.cpp
@@ -27,62 +27,62 @@ long StuntzHuntz::Explore(state_t *state, std::vector<state_t> *states)
 	long nc = 0;
 
 	// Calculate step distance based on nearby scores.
-	float step_dist = fmax(fmin(12/im.score(x,y,4), 400), 5);
-	float branch_num = 5*sqrt(step_dist);   // Scale with square root of step_dist. Scaling linearly costs too much time.
+	//float step_dist = fmax(fmin(12/im.score(x,y,4), 400), 5);
+	//float branch_num = 5*sqrt(step_dist);   // Scale with square root of step_dist. Scaling linearly costs too much time.
 
 	// TODO(yoos): Okay, might want to move step_dist back to decisiontree if this works.
-	while (states->size() == 0) {
-		nc = 0;
-		// Generate future states by distance and angle.
-		std::vector<state_t> probe_states;
-		for (int i=0; i<branch_num; i++) {
-			int dx = (step_dist * cos(2*M_PI/branch_num*i));
-			int dy = (step_dist * sin(2*M_PI/branch_num*i));
-			if (im.depth(x+dx, y+dy) >= 0) {
-				nc += AddDecision(&probe_states, x+dx, y+dy, CalcScore(state), state->budget-step_dist);
-			}
-		}
-
-		// Check if probed state satisfies criteria (TODO: what?)
-		for (std::vector<state_t>::iterator it=probe_states.begin(); it!=probe_states.end(); it++) {
-			if (im.score(it->loc.x, it->loc.y, 2) > 2.0) {
-				states->push_back(*it);
-			}
-		}
-
-		// Increase step_dist to try and find better pasture.
-		// TODO(yoos): What if we never do?
-		step_dist += 5;
-		branch_num = 5*sqrt(step_dist);
-		if (step_dist > 500) {
-			// Can't find good values, so just go somewhere.
-			step_dist = 20;   // ARBITRARY?
-			branch_num = 5*sqrt(step_dist);   // Scale with square root of step_dist. Scaling linearly costs too much time.
-
-			// Generate future states by distance and angle.
-			for (int i=0; i<branch_num; i++) {
-				int dx = (step_dist * cos(2*M_PI/branch_num*i));
-				int dy = (step_dist * sin(2*M_PI/branch_num*i));
-				if (im.depth(x+dx, y+dy) >= 0) {
-					nc += AddDecision(states, x+dx, y+dy, CalcScore(state), state->budget-step_dist);
-				}
-			}
-			break;
-		}
-	}
-
-	// Generate future states by distance from current location.
-	//const int radius = 10;
-	//const float tgt_dist = 10.0;
-	//for (int i=-radius; i<=radius; i++) {
-	//	for (int j=-radius; j<=radius; j++) {
-	//		float dist = sqrt(pow(i,2)+pow(j,2));
-	//		if (im.depth(x+i, y+j) >= 0 && (fabs(tgt_dist-dist) < 0.5)) {
-	//			//std::cout << x+i << ", " << y+j << "\n";
-	//			nc += AddDecision(states, x+i, y+j, CalcScore(state), state->budget-dist);
+	//while (states->size() == 0) {
+	//	nc = 0;
+	//	// Generate future states by distance and angle.
+	//	std::vector<state_t> probe_states;
+	//	for (int i=0; i<branch_num; i++) {
+	//		int dx = (step_dist * cos(2*M_PI/branch_num*i));
+	//		int dy = (step_dist * sin(2*M_PI/branch_num*i));
+	//		if (im.depth(x+dx, y+dy) >= 0) {
+	//			nc += AddDecision(&probe_states, x+dx, y+dy, CalcScore(state), state->budget-step_dist);
 	//		}
 	//	}
+
+	//	// Check if probed state satisfies criteria (TODO: what?)
+	//	for (std::vector<state_t>::iterator it=probe_states.begin(); it!=probe_states.end(); it++) {
+	//		if (im.score(it->loc.x, it->loc.y, 2) > 2.0) {
+	//			states->push_back(*it);
+	//		}
+	//	}
+
+	//	// Increase step_dist to try and find better pasture.
+	//	// TODO(yoos): What if we never do?
+	//	step_dist += 5;
+	//	branch_num = 5*sqrt(step_dist);
+	//	if (step_dist > 500) {
+	//		// Can't find good values, so just go somewhere.
+	//		step_dist = 20;   // ARBITRARY?
+	//		branch_num = 5*sqrt(step_dist);   // Scale with square root of step_dist. Scaling linearly costs too much time.
+
+	//		// Generate future states by distance and angle.
+	//		for (int i=0; i<branch_num; i++) {
+	//			int dx = (step_dist * cos(2*M_PI/branch_num*i));
+	//			int dy = (step_dist * sin(2*M_PI/branch_num*i));
+	//			if (im.depth(x+dx, y+dy) >= 0) {
+	//				nc += AddDecision(states, x+dx, y+dy, CalcScore(state), state->budget-step_dist);
+	//			}
+	//		}
+	//		break;
+	//	}
 	//}
+
+	// Generate future states by distance from current location.
+	const int radius = 5;
+	const float tgt_dist = 5.0;
+	for (int i=-radius; i<=radius; i++) {
+		for (int j=-radius; j<=radius; j++) {
+			float dist = sqrt(pow(i,2)+pow(j,2));
+			if (im.depth(x+i, y+j) >= 0 && (fabs(tgt_dist-dist) < 0.5)) {
+				//std::cout << x+i << ", " << y+j << "\n";
+				nc += AddDecision(states, x+i, y+j, CalcScore(state), state->budget-dist);
+			}
+		}
+	}
 
 	// Depreciate cost in and around visited location.
 	DepreciateScore(state);
