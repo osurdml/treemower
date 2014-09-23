dab3e9c69ea910141352addf3e1dcd04efbf3a0e
Soo-Hyun Yoo <yoos117@gmail.com>
2014-09-23 04:25:13 -0700

    refs #26 Comment on config values.

--- include/config.hpp
+++ include/config.hpp
@@ -1,10 +1,29 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
+// Start location
+const long START_X = 250;
+const long START_Y = 250;
+
+// Uncertainty reduction base factor, i.e., exactly at sample point
 const float UNCERTAINTY_REDUCE_FACTOR = 0.5;
+
+// Exponential reduction factor. For a given radius away from the sample point,
+// the uncertainty reduction factor above will be multiplied by this factor
+// floor(radius) times.
+const float UNCERTAINTY_REDUCE_EXP = 0.9;
+
+// Sample radius.
 const long SAMPLE_RADIUS = 3;
+
+// Number of cells between sample points.
 const long SAMPLE_INTERVAL = 5;
+
+// Somewhat of a magic number. The number of branches generated is equal to
+// this times the square root of the step distance.
 const long BRANCH_FACTOR = 5;
+
+// Uncertainty below this is "good enough."
 const float UNCERTAINTY_THRESHOLD = 2.0;
 
 #endif // CONFIG_HPP
