7805096195bcbcb1e32f80c4ce674323285fa1c3
Soo-Hyun Yoo <yoos117@gmail.com>
2014-09-22 17:50:45 -0700

    Update plotpath to work more like animplot.

--- vis/plotpath.m
+++ vis/plotpath.m
@@ -1,24 +1,15 @@
-clear
-close all
-scoremap = csvread('../data/costmap2.txt');
-M1 = csvread('../output/lm.out');
-M2 = csvread('../output/sh.out');
-x1 = M1(:,2);
-y1 = M1(:,1);
-x2 = M2(:,2);
-y2 = M2(:,1);
-map = zeros(100,100);
-figure;
-contour(scoremap, 30);
-hold on
-grid on
-xlabel('X');
-ylabel('Y','Rotation',0);
-line(x1,y1,'Color','b','LineWidth',1,'Marker','.');
-figure;
-contour(scoremap, 30);
-hold on
-grid on
-xlabel('X');
-ylabel('Y','Rotation',0);
-line(x2,y2,'Color','b','LineWidth',1,'Marker','.');
+function exitcode = plotpath(scoremap_fn, path_fn)
+	scoremap = csvread(scoremap_fn);
+	M = csvread(path_fn);
+	x = M(:,2);
+	y = M(:,1);
+	map = zeros(100,100);
+
+	figure;
+	contour(scoremap, 50);
+	hold on
+	grid on
+	xlabel('X');
+	ylabel('Y');
+	line(x,y,'Color','b','LineWidth',1,'Marker','.');
+
