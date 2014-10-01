5b10c3e07dc38c83bf2b67e4f8e897c6fc023376
yawei1 <WSSIL1022>
2014-10-01 14:20:26 -0700

    fix readMP.m

--- tools/readMP.m
+++ tools/readMP.m
@@ -5,7 +5,7 @@ for i = 5:5:30
         map_filename = strcat('500x500_',int2str(i),'_',int2str(j),'.map');
         for rf = 0.2:0.2:1.0
             for th = 0.0:0.05:0.25
-                polt_filename = strcat('500x500_',int2str(i),'_',int2str(j),'_rh_b25000_la2_rf',num2str(rf),'_th',num2str(th),'.path');
+                path_filename = strcat('500x500_',int2str(i),'_',int2str(j),'_rh_b25000_la2_rf',num2str(rf),'_th',num2str(th),'.path');
                 plotpath(map_filename,path_filename);
             end
         end
